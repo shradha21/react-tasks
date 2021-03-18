c061d6da05ce42b4fd0ac7dbdf894ce428612b3c
shradha <44254743+shradha21@users.noreply.github.com>
2021-03-18 22:54:53 +0530

    Update configureStore.js

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,10 @@
 import {createStore,combineReducers,applyMiddleware} from 'redux'
 import Thunk from 'redux-thunk'
+import searchAction from '../actions/searchAction'
 
 const configureStore=()=>{
     const store=createStore(combineReducers({
-        
+        search: searchReducer
     }),applyMiddleware(Thunk))
     return store
 }
